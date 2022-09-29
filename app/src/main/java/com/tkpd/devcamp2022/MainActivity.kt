bb179247a0b49b6ffc9061667f46f5d641adb509
said.faisal <said.faisal@tokopedia.com>
2022-09-29 21:47:59 +0700

    add demo code for customview and animation

--- app/src/main/java/com/tkpd/devcamp2022/MainActivity.kt
+++ app/src/main/java/com/tkpd/devcamp2022/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.tkpd.devcamp2022.databinding.ActivityMainBinding
 import com.tkpd.devcamp2022.day3.connecting_to_internet.weatherapp.presentation.WeatherActivity
+import com.tkpd.devcamp2022.day4.custom_view_animation.AnimationViewActivity
+import com.tkpd.devcamp2022.day4.custom_view_animation.CustomViewActivity
 import com.tkpd.devcamp2022.day4.unit_test_instrument_test.presentation.ui.ContactBookActivity
 
 class MainActivity: AppCompatActivity() {
@@ -18,6 +20,14 @@ class MainActivity: AppCompatActivity() {
 
         binding.btnDay3.setOnClickListener {
             startActivity(Intent(this@MainActivity, WeatherActivity::class.java))
+        }
+
+        binding.btnDay4Customview.setOnClickListener {
+            startActivity(Intent(this@MainActivity, CustomViewActivity::class.java))
+        }
+
+        binding.btnDay4Animation.setOnClickListener {
+            startActivity(Intent(this@MainActivity, AnimationViewActivity::class.java))
         }
 
         binding.btnDay4.setOnClickListener {
---
+++ app/src/main/java/com/tkpd/devcamp2022/day4/custom_view_animation/AnimationViewActivity.kt
@@ -0,0 +1,37 @@
+package com.tkpd.devcamp2022.day4.custom_view_animation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityOptionsCompat
+import com.tkpd.devcamp2022.databinding.ActivityAnimationViewBinding
+
+class AnimationViewActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_NAME = "name"
+        const val EXTRA_DETAIL = "detail"
+    }
+
+    lateinit var binding: ActivityAnimationViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAnimationViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.customvWireWord.onWiseWordClickListener = {
+            val intent = Intent(this, CustomViewAnimationDetailActivity::class.java)
+            intent.putExtra(EXTRA_NAME, it.name)
+            intent.putExtra(EXTRA_DETAIL, it.description)
+
+            val optionsCompat: ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                this,
+                androidx.core.util.Pair(binding.customvWireWord.tvNameWiseWord, "name"),
+                androidx.core.util.Pair(binding.customvWireWord.tvDescriptionWiseWord, "description")
+            )
+            startActivity(intent, optionsCompat.toBundle())
+//            startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(this).toBundle())
+        }
+    }
+}
