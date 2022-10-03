f068caf338b95d4be22b06402d84d29691fdc734
firmankagami <firmanda.nugroho@tokopedia.com>
2022-10-03 16:51:34 +0700

    feat: add example for preference datastore

--- app/src/main/java/com/tkpd/devcamp2022/day3/room_datastore/view/fragment/LocalDataFragment.kt
+++ app/src/main/java/com/tkpd/devcamp2022/day3/room_datastore/view/fragment/LocalDataFragment.kt
@@ -13,12 +13,26 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import com.tkpd.devcamp2022.R
 import com.tkpd.devcamp2022.databinding.FragmentLocalDataBinding
 import com.tkpd.devcamp2022.day3.room_datastore.api.MockUserApi
+import com.tkpd.devcamp2022.day3.room_datastore.datastore.UserDataStoreManager
 import com.tkpd.devcamp2022.day3.room_datastore.model.User
 import com.tkpd.devcamp2022.day3.room_datastore.view.viewmodel.UserViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LocalDataFragment: Fragment() {
 
     private var binding: FragmentLocalDataBinding? = null
+    lateinit var dataStoreManager: UserDataStoreManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        context?.let {
+            dataStoreManager = UserDataStoreManager(it)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -52,6 +66,30 @@ class LocalDataFragment: Fragment() {
             )
         }
 
+        binding?.btnLogout?.setOnClickListener {
+            clearDataStore()
+        }
+
+        GlobalScope.launch(Dispatchers.IO) {
+            dataStoreManager.getUserDataStore().catch { e ->
+                e.printStackTrace()
+                withContext(Dispatchers.Main) {
+                    showLoginState()
+                    hideLogoutState()
+                }
+            }.collect{
+                withContext(Dispatchers.Main) {
+                    if (it.isLoggedIn) {
+                        showLogoutState()
+                        hideLoginState()
+                    } else {
+                        showLoginState()
+                        hideLogoutState()
+                    }
+                }
+            }
+        }
+
         viewModel.user.observe(viewLifecycleOwner) {
             hideProgressBar()
             isLoggedInSucceed(it)
@@ -62,8 +100,23 @@ class LocalDataFragment: Fragment() {
     private fun isLoggedInSucceed(user: User) {
         if (user.isLoggedIn) {
             showToaster(getString(R.string.datastore_login_succeed))
+            saveToDataStore(user)
+            hideLoginState()
+            showLogoutState()
         } else {
             showToaster(getString(R.string.datastore_login_failed))
+        }
+    }
+
+    private fun saveToDataStore(user: User) {
+        GlobalScope.launch(Dispatchers.IO) {
+            dataStoreManager.saveToDataStore(user)
+        }
+    }
+
+    private fun clearDataStore() {
+        GlobalScope.launch(Dispatchers.IO) {
+            dataStoreManager.clearUserDataStore()
         }
     }
 
@@ -77,5 +130,25 @@ class LocalDataFragment: Fragment() {
 
     private fun hideProgressBar() {
         binding?.progressBar?.visibility = View.GONE
+    }
+
+    private fun hideLoginState() {
+        binding?.etPassword?.visibility = View.GONE
+        binding?.etUsername?.visibility = View.GONE
+        binding?.btnLogin?.visibility = View.GONE
+    }
+
+    private fun showLoginState() {
+        binding?.etPassword?.visibility = View.VISIBLE
+        binding?.etUsername?.visibility = View.VISIBLE
+        binding?.btnLogin?.visibility = View.VISIBLE
+    }
+
+    private fun showLogoutState() {
+        binding?.btnLogout?.visibility = View.VISIBLE
+    }
+
+    private fun hideLogoutState() {
+        binding?.btnLogout?.visibility = View.GONE
     }
 }
