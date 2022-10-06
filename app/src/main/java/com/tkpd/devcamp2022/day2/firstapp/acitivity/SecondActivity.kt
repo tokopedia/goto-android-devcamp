3f1fba2c5fbee5208aad3159e20d49211457a5a6
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-10-06 11:47:07 +0800

    feat : create new activity for first app

---
+++ app/src/main/java/com/tkpd/devcamp2022/day2/firstapp/acitivity/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.tkpd.devcamp2022.day2.firstapp.acitivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tkpd.devcamp2022.R
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
