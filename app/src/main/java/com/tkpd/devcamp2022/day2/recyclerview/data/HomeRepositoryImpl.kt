96964e47ab1eaa55bf739fc3b3d5685293e60bb5
meyta.taliti <meyta.taliti@tokopedia.com>
2022-10-07 00:02:00 +0700

    project starter

--- app/src/main/java/com/tkpd/devcamp2022/day2/recyclerview/data/HomeRepositoryImpl.kt
+++ app/src/main/java/com/tkpd/devcamp2022/day2/recyclerview/data/HomeRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.tkpd.devcamp2022.day2.recyclerview.domain.HomeRepository
 import com.tkpd.devcamp2022.day2.recyclerview.presentation.uimodel.BannerUiModel
 import com.tkpd.devcamp2022.day2.recyclerview.presentation.uimodel.HomeUiModel
 import com.tkpd.devcamp2022.day2.recyclerview.presentation.uimodel.ProductUiModel
+import com.tkpd.devcamp2022.day2.recyclerview.presentation.uimodel.TitleUiModel
 
 class HomeRepositoryImpl(
     private val dataSource: HomeDataSource
@@ -15,30 +16,23 @@ class HomeRepositoryImpl(
     }
 
     override fun getBannerAndProducts(): List<HomeUiModel> {
-        val schema = dataSource.getSchema().toMutableList()
-        schema[0] = BannerUiModel(getListOfImage()) // replace with real banners
-        schema.removeAt(1) // remove empty product
-        schema.removeAt(2) // remove empty product
-        schema.addAll(getProducts((0 until 10).random())) // add real products
-        return schema
+        return mutableListOf<HomeUiModel>().apply {
+            add(BannerUiModel(getListOfImage()))
+            addAll(getProducts((0 until 10).random()))
+        }
     }
 
     override fun getInitialHomeData(): List<HomeUiModel> {
-        val schema = getBannerAndTitle().toMutableList()
-        schema.addAll(getProducts((0 until 10).random())) // add real products
-        return schema
+        return mutableListOf<HomeUiModel>().apply {
+            add(BannerUiModel(getListOfImage()))
+            add(TitleUiModel("Rekomendasi Untukmu"))
+            addAll(getProducts((0 until 10).random()))
+        }
     }
 
     override fun getProducts(page: Int): List<ProductUiModel> {
         val products = dataSource.getProducts()
         return if (page % 2 == 0) products.take(5)
         else products.takeLast(5)
-    }
-
-    private fun getBannerAndTitle(): List<HomeUiModel> {
-        val schema = dataSource.getSchema().toMutableList()
-        schema[0] = BannerUiModel(getListOfImage()) // replace with real banners
-        schema.removeAt(2) // remove empty product
-        return schema
     }
 }
