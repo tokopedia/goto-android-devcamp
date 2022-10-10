2a76d0d68458a39154b80b011fc4e619aad9275e
firmankagami <firmanda.nugroho@tokopedia.com>
2022-10-10 11:16:40 +0700

    feat: starter preference

--- app/src/main/java/com/tkpd/devcamp2022/day3/room_datastore/datastore/UserPreferenceDataStoreManager.kt
+++ app/src/main/java/com/tkpd/devcamp2022/day3/room_datastore/datastore/UserPreferenceDataStoreManager.kt
@@ -11,8 +11,8 @@ import com.tkpd.devcamp2022.day3.room_datastore.model.User
 import kotlinx.coroutines.flow.map
 
 //TODO(2,1) - Create Preference Datastore
-private const val USER_DATA_STORE = "USER_DATASTORE"
-private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = USER_DATA_STORE)
+//private const val USER_DATA_STORE = "USER_DATASTORE"
+//private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = USER_DATA_STORE)
 
 class UserPreferenceDataStoreManager(val context: Context) {
 
@@ -22,28 +22,28 @@ class UserPreferenceDataStoreManager(val context: Context) {
         val USERNAME = stringPreferencesKey("USERNAME")
     }
 
-    //TODO(2,2) - Read Preference Datastore
-    fun getUserPreferenceDataStore() = context.dataStore.data.map {
-        User(
-            id = it[USER_ID] ?: "",
-            isLoggedIn = it[IS_LOGGED_IN] ?: false,
-            userName = it[USERNAME] ?: ""
-        )
-    }
+    //TODO(2,2) - Create Read Preference Datastore
+//    fun getUserPreferenceDataStore() = context.dataStore.data.map {
+//        User(
+//            id = it[USER_ID] ?: "",
+//            isLoggedIn = it[IS_LOGGED_IN] ?: false,
+//            userName = it[USERNAME] ?: ""
+//        )
+//    }
 
-    //TODO(2,3) - Save Preference Datastore
-    suspend fun saveToPreferenceDataStore(user: User) {
-        context.dataStore.edit {
-            it[USER_ID] = user.id
-            it[IS_LOGGED_IN] = user.isLoggedIn
-            it[USERNAME] = user.userName
-        }
-    }
+    //TODO(2,3) - Create Save Preference Datastore
+//    suspend fun saveToPreferenceDataStore(user: User) {
+//        context.dataStore.edit {
+//            it[USER_ID] = user.id
+//            it[IS_LOGGED_IN] = user.isLoggedIn
+//            it[USERNAME] = user.userName
+//        }
+//    }
 
-    //TODO(2,4) - Delete Preference Datastore
-    suspend fun clearUserPreferenceDataStore() {
-        context.dataStore.edit {
-            it.clear()
-        }
-    }
+    //TODO(2,4) - Create Delete Preference Datastore
+//    suspend fun clearUserPreferenceDataStore() {
+//        context.dataStore.edit {
+//            it.clear()
+//        }
+//    }
 }
