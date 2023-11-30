33fc2f1a63515d317f9674f47dfe44f489fd6167
Irpan - <irpan@tokopedia.com>
2023-11-30 11:03:31 +0700

    add bottomsheet detail news

--- app/src/main/java/com/tkpd/devcamp/customview_bottomsheet/SecondActivity.kt
+++ app/src/main/java/com/tkpd/devcamp/customview_bottomsheet/SecondActivity.kt
@@ -9,5 +9,20 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+
+        openBottomSheetDetail(
+            News(
+                "On the eve of Tesla's next major release, Elon Musk gave a bizarre, long-winded interview about everything but the Cybertruck",
+                "Zac Johnson",
+                "businessinsider.com",
+                "https://biztoc.com/x/c191194496ce4396",
+                "https://c.biztoc.com/p/c191194496ce4396/s.webp"
+            )
+        )
+    }
+
+    private fun openBottomSheetDetail(news: News) {
+        BottomSheetDetailNews.show(supportFragmentManager, news)
     }
 }
