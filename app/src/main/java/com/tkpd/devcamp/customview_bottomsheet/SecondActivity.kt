b236fa93921284ca0385cbf6f5def73dc530d3ab
calvinkurniawan <calvin.kurniawan@tokopedia.com>
2023-11-30 10:33:44 +0700

    add custom view news (#31)

--- app/src/main/java/com/tkpd/devcamp/customview_bottomsheet/SecondActivity.kt
+++ app/src/main/java/com/tkpd/devcamp/customview_bottomsheet/SecondActivity.kt
@@ -1,13 +1,39 @@
 package com.tkpd.devcamp.customview_bottomsheet
 
 import android.os.Bundle
+import android.widget.LinearLayout.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
-import com.tkpd.devcamp.R
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.tkpd.devcamp.customview_bottomsheet.customview.NewsCard
+import com.tkpd.devcamp.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.root.let {
+            it.addView(
+                NewsCard(this).apply {
+                    layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT).apply {
+                        setMargins(0, 0, 0, 20)
+                    }
+                }
+            )
+
+            it.addView(
+                NewsCard(
+                    this,
+                    newsTitle = "News Title",
+                    newsAuthor = "Me & Friends",
+                    newsDescription = "Lorem ipsum, testing news card"
+                )
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/tkpd/devcamp/customview_bottomsheet/customview/NewsCard.kt
@@ -0,0 +1,100 @@
+package com.tkpd.devcamp.customview_bottomsheet.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
+import com.tkpd.devcamp.R
+import com.tkpd.devcamp.databinding.NewsCardLayoutBinding
+
+class NewsCard : ConstraintLayout {
+    private val binding: NewsCardLayoutBinding =
+        NewsCardLayoutBinding.inflate(LayoutInflater.from(context), this, true)
+
+    // primary constructor
+    constructor(context: Context) : super(context) {
+        setupNewsCard()
+    }
+
+    // constructor for XML implementation
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        initializeViewWithAttribute(attributeSet)
+    }
+
+    // constructor for initialize and set data
+    constructor(
+        context: Context,
+        newsTitle: String,
+        newsAuthor: String,
+        newsDescription: CharSequence,
+        newsImageUrl: String? = null
+    ) : super(context) {
+        setupNewsCard(newsTitle, newsAuthor, newsDescription, newsImageUrl)
+    }
+
+    private fun initializeViewWithAttribute(attributeSet: AttributeSet) {
+        val attributeArray = context.obtainStyledAttributes(attributeSet, R.styleable.NewsCard)
+
+        // get news_title value from xml
+        val newsTitle = attributeArray.getString(R.styleable.NewsCard_news_title)
+
+        // get news_author from xml
+        val newsAuthor = attributeArray.getString(R.styleable.NewsCard_news_author)
+
+        // get news_description from xml
+        val newsDescription = attributeArray.getString(R.styleable.NewsCard_news_description)
+
+        // get news_description from xml
+        val newsImageUrl = attributeArray.getString(R.styleable.NewsCard_news_img_url)
+
+        setupNewsCard(
+            newsTitle = newsTitle,
+            newsAuthor = newsAuthor,
+            newsDescription = newsDescription,
+            newsImageUrl = newsImageUrl
+        )
+
+        attributeArray.recycle()
+    }
+
+    private fun setupNewsCard(
+        newsTitle: String? = null,
+        newsAuthor: String? = null,
+        newsDescription: CharSequence? = null,
+        newsImageUrl: String? = null
+    ) {
+        setTitle(newsTitle ?: DEFAULT_TITLE)
+        setAuthor(newsAuthor ?: DEFAULT_AUTHOR)
+        setDescription(newsDescription ?: DEFAULT_DESCRIPTION)
+        newsImageUrl?.let {
+            setImageUrl(it)
+        }
+    }
+
+    fun setImageUrl(url: String) {
+        Glide.with(context)
+            .load(url)
+            .centerCrop()
+            .into(binding.contentImg)
+    }
+
+    fun setTitle(title: String) {
+        binding.contentTitle.text = title
+    }
+
+    fun setAuthor(author: String) {
+        binding.contentAuthor.text = author
+    }
+
+    fun setDescription(description: CharSequence) {
+        binding.contentDesc.text = description
+    }
+
+    companion object {
+        private const val DEFAULT_TITLE = "Title"
+        private const val DEFAULT_AUTHOR = "Author"
+        private const val DEFAULT_DESCRIPTION =
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+    }
+}
