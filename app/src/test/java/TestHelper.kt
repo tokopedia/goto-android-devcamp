ba3fa1cf204b8b059ca716169de0b1d11daf110f
astidhiyaa <asti.anzaria@tokopedia.com>
2023-12-07 10:01:38 +0700

    feat: add test case

--- app/src/test/java/TestHelper.kt
+++ app/src/test/java/TestHelper.kt
@@ -30,13 +30,3 @@ fun <T> LiveData<T>.getOrAwaitValue(
     @Suppress("UNCHECKED_CAST")
     return data as T
 }
-
-suspend fun <T> LiveData<T>.observeForTesting(block: suspend  () -> Unit) {
-    val observer = Observer<T> { }
-    try {
-        observeForever(observer)
-        block()
-    } finally {
-        removeObserver(observer)
-    }
-}
