41c19773ff73e459b56310780d20f564d46e0c4b
astidhiyaa <asti.anzaria@tokopedia.com>
2023-12-07 08:52:34 +0700

    feat: add test case

--- app/src/test/java/com/tkpd/devcamp/connect_to_internet/data/BasicTestCase.kt
+++ app/src/test/java/com/tkpd/devcamp/connect_to_internet/data/BasicTestCase.kt
@@ -1,25 +1,30 @@
 package com.tkpd.devcamp.connect_to_internet.data
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
-class BasicTestCase {
+class BasicTest {
     @Test
     fun `given is 2 is even number`() {
-        TODO("Not yet implemented")
-        //given
-        //when
-        //then
+        val number = 2
+        val magicNumbers = MagicNumbers()
+
+        val actual = magicNumbers.isEven(number)
+
+        assertEquals(true, actual)
     }
 
     @Test
     fun `given is 7 is even number`() {
-        TODO("Not yet implemented")
-        //given
-        //when
-        //then
+        val number = 7
+        val magicNumbers = MagicNumbers()
+
+        val actual = magicNumbers.isEven(number)
+
+        assertEquals(false, actual)
     }
 }
 
-class MagicNumbers{
+class MagicNumbers {
     fun isEven(number: Int): Boolean = number % 2 == 0
 }
